42a2e770b96407784b5af50e073fef87546232bc
Magomed Mustapaev <mustapaev760@gmail.com>
2025-07-02 19:10:43 +0300

    Добавлена таблица unit (ед.измерения).

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=vegetable-storage',
     'username' => 'root',
-    'password' => '6grkyxnt',
+    'password' => '1243',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
---
+++ migrations/m250702_155641_create_unit_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%unit}}`.
+ */
+class m250702_155641_create_unit_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%unit}}', [
+            'id' => $this->primaryKey()->comment('ID'),
+            'name' => $this->string(100)->notNull()->comment('Название'),
+            'is_weight' => $this->smallInteger(1)->notNull()->defaultValue(0)->comment('Весовая'),
+            'weight' => $this->decimal(10,3)->null()->comment('Вес'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%unit}}');
+    }
+}
