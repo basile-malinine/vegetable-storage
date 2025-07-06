07f1326314d34553c0c8debc27e3d429b745efc3
Basile <basile-m@yandex.ru>
2025-07-06 09:49:21 +0300

    Правка миграции для таблицы Пользователей.

--- migrations/m241210_074641_create_user_table.php
+++ migrations/m241210_074641_create_user_table.php
@@ -3,28 +3,33 @@
 use yii\db\Migration;
 
 /**
- * Class m241210_074641_create_table_user
+ * Class m241210_074641_create_user_table
  */
-class m241210_074641_create_table_user extends Migration
+class m241210_074641_create_user_table extends Migration
 {
     /**
      * {@inheritdoc}
      */
-    public function up()
+    public function safeUp()
     {
-        $this->createTable('user', [
+        $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull()->comment('Имя пользователя'),
             'email' => $this->string()->notNull()->unique()->comment('E-mail'),
             'pass_hash' => $this->string()->notNull()->comment('Пароль'),
+        ]);
+        $this->insert('{{%user}}', [
+            'name' => 'admin',
+            'email' => 'admin@mail.net',
+            'pass_hash' => Yii::$app->getSecurity()->generatePasswordHash('admin'),
         ]);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function down()
+    public function safeDown()
     {
-        $this->dropTable('user');
+        $this->dropTable('{{%user}}');
     }
 }
